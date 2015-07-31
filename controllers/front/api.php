7f0ff9df3dbd08d0766cf2331c61e2f780a6d3cc
Daniel Hoffmann <daniel.hoffmann@jtl-software.com>
2015-07-31 12:02:44 +0200

    optimized presta module structure cleaned up build process

--- controllers/front/api.php
+++ controllers/front/api.php
@@ -4,9 +4,13 @@ class jtlconnectorApiModuleFrontController extends ModuleFrontController
 {
     public function initContent()
     {
-        require_once realpath(__DIR__ . "/../../vendor/autoload.php");
-
         defined('CONNECTOR_DIR') || define("CONNECTOR_DIR", realpath(__DIR__.'/../../'));
+
+        if (file_exists(CONNECTOR_DIR.'/vendor/autoload.php')) {
+            require_once CONNECTOR_DIR.'/vendor/autoload.php';
+        } else {
+            include_once 'phar://'.CONNECTOR_DIR.'/connector.phar';
+        }
 
         $connector = \jtl\Connector\Presta\Presta::getInstance();
 
--- stub.php
+++ stub.php
@@ -1,12 +1,6 @@
 <?php
-if (file_exists(__DIR__.'/include.php')) {
-    include(__DIR__.'/include.php');
-}
+Phar::mapPhar('connector.phar');
 
-Phar::mapPhar('jtlconnector.php');
-
-defined('CONNECTOR_DIR') || define('CONNECTOR_DIR', __DIR__);
-
-include_once 'phar://jtlconnector.php/jtlconnector.php';
+include_once 'phar://connector.phar/vendor/autoload.php';
 
 __HALT_COMPILER();
