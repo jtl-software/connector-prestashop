c3284469fe0e4c3beee77e3f5f40ed252cbe472f
Daniel Hoffmann <daniel.hoffmann@jtl-software.com>
2015-08-14 08:41:30 +0200

    added shipping item for CustomerOrders fixed features.json

--- src/jtl/Connector/Presta/Controller/CustomerOrderItem.php
+++ src/jtl/Connector/Presta/Controller/CustomerOrderItem.php
@@ -1,6 +1,9 @@
 <?php
 namespace jtl\Connector\Presta\Controller;
 
+use jtl\Connector\Model\CustomerOrderItem as CustomerorderItemModel;
+use jtl\Connector\Model\Identity;
+
 class CustomerOrderItem extends BaseController
 {
     public function pullData($data, $model, $limit = null)
@@ -18,6 +21,16 @@ class CustomerOrderItem extends BaseController
 
             $return[] = $model;
         }
+
+        $shipping = new CustomerOrderItemModel();
+        $shipping->setId(new Identity('shipping_'.$data['id_order']));
+        $shipping->setCustomerOrderId(new Identity($data['id_order']));
+        $shipping->setType('shipping');
+        $shipping->setName($data['shippingMethod']);
+        $shipping->setPrice(floatval($data['total_shipping_tax_excl']));
+        $shipping->setVat(floatval($data['carrier_tax_rate']));
+
+        $return[] = $shipping;
 
         return $return;
     }
