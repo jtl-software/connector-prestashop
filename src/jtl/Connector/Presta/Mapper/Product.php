acfe48239e3d6d77cfb03c237746d22806dd51f3
Jan Weskamp <jan.weskamp@jtl-software.com>
2018-10-10 10:21:18 +0200

    -fix build sh

--- src/jtl/Connector/Presta/Mapper/Product.php
+++ src/jtl/Connector/Presta/Mapper/Product.php
@@ -108,6 +108,8 @@ class Product extends BaseMapper
         if (isset($data['id_product_attribute'])) {
             return new Identity($data['id_product']);
         }
+        
+        return null;
     }
     
     protected function id_category_default($data)
