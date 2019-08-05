ccb9faba2372fd19fa9d9c7c3186698baa1b6527
Benjamin Schaffrath <benjamin.schaffrath@jtl-software.com>
2019-08-05 11:13:11 +0200

    Pushing current testing status

--- tests/ConnectorTestCase.php
+++ tests/ConnectorTestCase.php
@@ -2,13 +2,19 @@
 
 namespace Tests;
 
+use jtl\Connector\Model\DataModel;
+use jtl\Connector\Serializer\JMS\SerializerBuilder;
 use PHPUnit\Framework\TestCase;
 use Jtl\Connector\Client\Client;
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
 
 class ConnectorTestCase extends TestCase
 {
     protected static $client = null;
     
+    /**
+     * @return Client
+     */
     protected function getConnectorClient()
     {
         if (self::$client === null) {
@@ -20,10 +26,94 @@ class ConnectorTestCase extends TestCase
         return self::$client;
     }
     
+    /**
+     * @return Client
+     */
     private function generateClient()
     {
-        $config = json_decode(file_get_contents('test-config.json'));
+        $config = json_decode(file_get_contents(__DIR__ . '/test-config.json'));
         
-        return new Client($config['connector-url'], $config['connector-token']);
+        return new Client($config->connector_token, $config->connector_url);
+    }
+    
+    /**
+     * @param string $json
+     * @param string $controllerName
+     * @return array|\JMS\Serializer\scalar|mixed|object
+     */
+    protected function jsonToCoreModels(string $json, string $controllerName)
+    {
+        $ns = 'ArrayCollection<jtl\\Connector\\Model\\' . $controllerName . '>';
+        $serializer = SerializerBuilder::create();
+        return $serializer->deserialize($json, $ns, 'json');
+    }
+    
+    /**
+     * @param string $modelName
+     * @param int $limit
+     * @param string $endpointId
+     * @return DataModel|DataModel[]|null
+     */
+    protected function pullCoreModels(string $modelName, int $limit = 100, string $endpointId = "")
+    {
+        if ($endpointId === "") {
+            return $this->getConnectorClient()->pull($modelName, $limit);
+        }
+        
+        $models = $this->getConnectorClient()->pull($modelName, 999999);
+        foreach ($models as $model) {
+            if ($model->getId()->getEndpoint() === $endpointId) {
+                return $model;
+            }
+        }
+        
+        return null;
+    }
+    
+    /**
+     * @param array $models
+     * @return array
+     * @throws \ReflectionException
+     */
+    protected function pushCoreModels(array $models)
+    {
+        if (empty($models)) {
+            return [];
+        }
+        
+        $controllerName = (new \ReflectionClass($models[0]))->getShortName();
+        $client = $this->getConnectorClient();
+        
+        
+        return $this->jsonToCoreModels(json_encode($client->push($controllerName, $models)), $controllerName);
+    }
+    
+    /**
+     * @param DataModel $actual
+     * @param array $assertArray
+     * @param DataModel $expected
+     */
+    //protected function assertCoreModel(DataModel $model, array $assertArray, DataModel $assertModel = null)
+    protected function assertCoreModel(DataModel $actual, DataModel $expected, array $assertArray = null)
+    {
+        if (empty($assertArray)) {
+            $this->assertEquals($expected, $actual);
+            
+            return;
+        }
+        
+        $valid = [
+            'assertEquals',
+            'assertSame',
+        ];
+        
+        foreach ($assertArray as $name => $assertion) {
+            $assertMethod = "assert".ucfirst($assertion);
+            $getMethod = "get".ucfirst($name);
+            
+            if(array_search($assertMethod, $valid)  !== false) {
+                $this->$assertMethod($actual->$getMethod(), $expected->$getMethod());
+            }
+        }
     }
 }
--- tests/Integration/ProductTest.php
+++ tests/Integration/ProductTest.php
@@ -2,7 +2,12 @@
 
 namespace Tests\Integration;
 
+use jtl\Connector\Model\Identity;
 use jtl\Connector\Model\Product;
+use jtl\Connector\Model\ProductI18n;
+use jtl\Connector\Model\ProductPrice;
+use jtl\Connector\Model\ProductStockLevel;
+use jtl\Connector\Serializer\JMS\SerializerBuilder;
 use Tests\ConnectorTestCase;
 
 class ProductTest extends ConnectorTestCase
@@ -14,7 +19,31 @@ class ProductTest extends ConnectorTestCase
     public function testProductPush()
     {
         $product = new Product();
-        var_dump($product->toJson());
+        $product->getId()->setHost(1);
+        $product->setStockLevel(new ProductStockLevel());
+        $product->addPrice(new ProductPrice());
+        $productI18N = new ProductI18n();
+        $productI18N->setLanguageISO('de');
+        $productI18N->setDescription('Normale Beschreibung');
+        $productI18N->setName("Produktname");
+        $productI18N->setProductId($product->getId());
+        $productI18N->setShortDescription('Kurze Beschreibung');
+        $product->addI18n($productI18N);
+        
+        $response = $this->pushCoreModels([$product]);
+        
+        $i18nAssertions = [
+            'LanguageISO' => 'Equals',
+            'Description' => 'Equals',
+            'Name' => 'Equals',
+        ];
+        
+        $endpointId = $response[0]->getId()->getEndpoint();
+        $this->assertNotEmpty($endpointId);
+        
+        $response[0]->getI18ns()[0]->getProductId()->setEndpoint('');
+        
+        $this->assertCoreModel($product->getI18ns()[0], $response[0]->getI18ns()[0]);//, $i18nAssertions);
     }
     
     public function productDataProvider()
