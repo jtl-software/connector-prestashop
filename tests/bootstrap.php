d4b29ba266e626f8c5565f8a32e4cd0294663011
Benjamin Schaffrath <benjamin.schaffrath@jtl-software.com>
2019-08-15 14:57:53 +0200

    Fix for test-config location

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@ use jtl\Connector\Presta\Mapper\PrimaryKeyMapper;
 require '../../config/config.inc.php';
 $loader = require 'vendor/autoload.php';
 
+const TEST_DIR = __DIR__;
+
 function getPrimaryKeyMapper()
 {
     return new PrimaryKeyMapper();
