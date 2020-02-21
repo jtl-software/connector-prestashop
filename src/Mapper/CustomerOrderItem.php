52bf061f0242c21a991c1a3092c8651b4af7a706
Patryk Gorol <patryk.gorol@jtl-software.com>
2020-02-21 11:31:24 +0100

    Added tax rate query for tax rate check Changed net price column mapping

--- src/Mapper/CustomerOrderItem.php
+++ src/Mapper/CustomerOrderItem.php
@@ -11,7 +11,7 @@ class CustomerOrderItem extends BaseMapper
         'productId' => null,
         'customerOrderId' => 'id_order',
         'name' => 'product_name',
-        'price' => 'product_price',
+        'price' => 'unit_price_tax_excl',
         'priceGross' => 'unit_price_tax_incl',
         'quantity' => 'product_quantity',
         'sku' => 'product_reference',
@@ -22,13 +22,27 @@ class CustomerOrderItem extends BaseMapper
     {
         return new Identity($data['product_attribute_id'] == 0 ? $data['product_id'] :  $data['product_id'].'_'.$data['product_attribute_id']);
     }
-    
+
     protected function vat($data)
     {
         if ((float)$data['total_price_tax_excl'] === (float)0) {
             return 0;
         }
-        
-        return round(floatval($data['tax_rate'] == 0 ? (100 / $data['total_price_tax_excl'] * $data['total_price_tax_incl']) - 100 : $data['tax_rate']), 4);
+
+        $rate = $data['tax_rate'];
+
+        if ($rate == 0) {
+            $query = sprintf('SELECT t.rate FROM %stax t 
+                         LEFT JOIN %sorder_detail_tax dt ON dt.id_tax = t.id_tax
+                         WHERE dt.id_order_detail = %s', _DB_PREFIX_, _DB_PREFIX_, $data['id_order_detail']);
+
+            $rate = $this->db->getValue($query);
+
+            if ($rate === false || $rate === null) {
+                $rate = (100 / $data['total_price_tax_excl'] * $data['total_price_tax_incl']) - 100;
+            }
+        }
+
+        return round((float)$rate, 4);
     }
 }
