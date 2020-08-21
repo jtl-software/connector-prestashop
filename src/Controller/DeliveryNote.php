d11b8652b308ad32a056c2ae61c61d8e55ad30ab
Niklas Kokoschka <niklas.kokoschka@jtl-software.com>
2020-08-21 11:36:18 +0200

    Fixed review notices

--- src/Controller/DeliveryNote.php
+++ src/Controller/DeliveryNote.php
@@ -7,17 +7,24 @@ class DeliveryNote extends BaseController
     public function pushData($data)
     {
         $orderId = $data->getCustomerOrderId()->getEndpoint();
-
+    
         if (!empty($orderId)) {
-            $trackingIds = implode(', ', $data->getTrackingLists()[0]->getCodes());
+            $trackingCodes = [];
+            foreach ($data->getTrackingLists() as $trackingList) {
+                $trackingCodes = array_merge($trackingCodes, $trackingList->getCodes());
+            }
             
-            $this->db->execute(sprintf('UPDATE %sorder_carrier SET tracking_number=%s WHERE id_order=%s',
-                _DB_PREFIX_,
-                $trackingIds,
-                $orderId
-            ));
+            if (count($trackingCodes) > 0) {
+                $this->db->execute(sprintf('UPDATE %sorder_carrier SET tracking_number=CONCAT(tracking_number, ", ", "%s") WHERE id_order=%s',
+                    _DB_PREFIX_,
+                    implode(', ', $trackingCodes),
+                    $orderId
+                ));
+            }
         }
-
+    
         return $data;
     }
 }
+
+
