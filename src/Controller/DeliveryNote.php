a987505ef6e3c3670938a5b4334bc7b835aee51f
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2020-10-13 10:40:52 +0200

    Fixed updating tracking codes

--- src/Controller/DeliveryNote.php
+++ src/Controller/DeliveryNote.php
@@ -7,23 +7,26 @@ class DeliveryNote extends BaseController
     public function pushData($data)
     {
         $orderId = $data->getCustomerOrderId()->getEndpoint();
-    
+
         if (!empty($orderId)) {
             $trackingCodes = [];
             foreach ($data->getTrackingLists() as $trackingList) {
                 $trackingCodes = array_merge($trackingCodes, $trackingList->getCodes());
             }
-            
+
+            $codesString = implode(', ', $trackingCodes);
+
             if (count($trackingCodes) > 0) {
                 $this->db->execute(sprintf('UPDATE %sorder_carrier SET tracking_number=
-                    CONCAT_WS(", ", IF(LENGTH(tracking_number),tracking_number, NULL), "%s") WHERE id_order=%s',
+                    IF(LENGTH(tracking_number) > 0, CONCAT_WS(", ", tracking_number, "%s"), "%s") WHERE id_order=%s',
                     _DB_PREFIX_,
-                    implode(', ', $trackingCodes),
+                    $codesString,
+                    $codesString,
                     $orderId
                 ));
             }
         }
-    
+
         return $data;
     }
 }
