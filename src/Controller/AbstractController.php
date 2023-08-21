2dc5a1703989d8d35a53cbb12826fb02a84f6ff3
Patrick Gugelsberger <pgugelsberger@gmail.com>
2023-08-21 15:39:28 +0000

    category controller interim

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -3,6 +3,8 @@
 namespace jtl\Connector\Presta\Controller;
 
 use DI\Container;
+use jtl\Connector\Presta\Utils\QueryBuilder;
+use PrestaShopDatabaseException;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
@@ -44,16 +46,32 @@ abstract class AbstractController implements LoggerAwareInterface
         $this->logger = $logger;
     }
 
-    protected function getPrestaLanguage(int $langId): string
+    /**
+     * @param int $langId
+     * @return string
+     * @throws PrestaShopDatabaseException
+     */
+    protected function getJtlLanguageIsoFromLanguageId(int $langId): string
     {
-        $result = $this->db->executeS(
-            'SELECT iso_code
-            FROM ' . \_DB_PREFIX_ . 'lang
-            WHERE id_lang = ' . $langId
-        );
+        $sql = (new QueryBuilder())
+            ->select('iso_code')
+            ->from('lang')
+            ->where("id_lang = $langId");
+
+        $result = $this->db->executeS($sql);
 
         $linguaConverter = Service::createFromISO_639_1($result[0]['iso_code']);
 
         return $linguaConverter->toISO_639_2b();
+    }
+
+    protected function getPrestaLanguageIdFromIso(string $languageIso): int
+    {
+        $sql = (new QueryBuilder())
+            ->select('id_lang')
+            ->from('lang')
+            ->where("iso_code = '$languageIso'");
+
+        return $this->db->executeS($sql)[0]['id_lang'];
     }
 }
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace jtl\Connector\Presta\Controller;
 
+use Category as PrestaCategory;
 use Jtl\Connector\Core\Controller\DeleteInterface;
 use Jtl\Connector\Core\Controller\PullInterface;
 use Jtl\Connector\Core\Controller\PushInterface;
@@ -10,6 +11,9 @@ use Jtl\Connector\Core\Model\CategoryI18n as JtlCategoryI18n;
 use Jtl\Connector\Core\Model\Identity;
 use Jtl\Connector\Core\Model\QueryFilter;
 use Jtl\Connector\Core\Model\AbstractModel;
+use Jtl\Connector\Core\Model\Statistic;
+use jtl\Connector\Presta\Utils\QueryBuilder;
+use PrestaShopException;
 
 class CategoryController extends AbstractController implements PullInterface, PushInterface, DeleteInterface
 {
@@ -17,16 +21,18 @@ class CategoryController extends AbstractController implements PullInterface, Pu
 
     public function pull(QueryFilter $queryFilter): array
     {
+        $queryBuilder = new QueryBuilder();
+        $queryBuilder->setUsePrefix(false);
 
-        $prestaCategories = $this->db->executeS(
-            '
-			SELECT c.* 
-			FROM ' . \_DB_PREFIX_ . 'category c
-			LEFT JOIN jtl_connector_link_category l ON c.id_category = l.endpoint_id
-            WHERE l.host_id IS NULL AND c.id_parent != 0 AND c.is_root_category = 0
-            ORDER BY c.nleft
-            LIMIT ' . $queryFilter->getLimit()
-        );
+        $sql = $queryBuilder
+            ->select('c.*')
+            ->from(\_DB_PREFIX_ . 'category', 'c')
+            ->leftJoin('jtl_connector_link_category', 'l', 'c.id_category = l.endpoint_id')
+            ->where('l.host_id IS NULL AND c.id_parent != 0 AND c.is_root_category = 0')
+            ->orderBy('c.nleft')
+            ->limit($this->db->escape($queryFilter->getLimit()));
+
+        $prestaCategories = $this->db->executeS($sql);
 
         $jtlCategories = [];
 
@@ -66,21 +72,20 @@ class CategoryController extends AbstractController implements PullInterface, Pu
             ->setDescription($prestaCategory['description'])
             ->setMetaDescription($prestaCategory['meta_description'])
             ->setMetaKeywords($prestaCategory['meta_keywords'])
-            ->setLanguageIso($this->getPrestaLanguage($prestaCategory['id_lang']));
+            ->setLanguageIso($this->getJtlLanguageIsoFromLanguageId($prestaCategory['id_lang']));
     }
 
     protected function createJtlCategoryTranslations(int $prestaCategoryId): array
     {
-        $results = $this->db->executeS(
-            \sprintf(
-                'SELECT cl.*
-			FROM ' . \_DB_PREFIX_ . 'category_lang cl
-			LEFT JOIN ' . \_DB_PREFIX_ . 'lang AS l ON l.id_lang = cl.id_lang
-			WHERE cl.id_category = %s AND cl.id_shop = %s',
-                $prestaCategoryId,
-                \Context::getContext()->shop->id
-            )
-        );
+        $shopId = \Context::getContext()->shop->id;
+
+        $sql = (new QueryBuilder())
+            ->select('cl.*')
+            ->from('category_lang', 'cl')
+            ->leftJoin('lang', 'l', 'l.id_lang = cl.id_lang')
+            ->where("cl.id_category = $prestaCategoryId AND cl.id_shop = $shopId");
+
+        $results = $this->db->executeS($sql);
 
         $i18ns = [];
 
@@ -91,44 +96,92 @@ class CategoryController extends AbstractController implements PullInterface, Pu
         return $i18ns;
     }
 
-    public function push(AbstractModel $model): AbstractModel
+    public function push(AbstractModel $jtlCategory): AbstractModel
     {
-        if (isset(static::$idCache[$data->getParentCategoryId()->getHost()])) {
-            $data->getParentCategoryId()->setEndpoint(static::$idCache[$data->getParentCategoryId()->getHost()]);
+        $queryBuilder = new QueryBuilder();
+        $queryBuilder->setUsePrefix(false);
+
+        $prestaCategory = new PrestaCategory();
+        $endpoint       = $jtlCategory->getId()->getEndpoint();
+        $isNew          = $endpoint === '';
+
+        if (!$isNew) {
+            $prestaCategory = $this->createPrestaCategory($jtlCategory, new PrestaCategory($endpoint));
+            $prestaCategory->update();
         }
 
-        $category = $this->mapper->toEndpoint($data);
-        $category->save();
+        $prestaCategory = $this->createPrestaCategory($jtlCategory, $prestaCategory);
+        $prestaCategory->add();
 
-        $id = $category->id;
 
-        $data->getId()->setEndpoint($id);
-
-        static::$idCache[$data->getId()->getHost()] = $id;
-
-        return $data;
+        return $jtlCategory;
     }
 
+    protected function createPrestaCategory(JtlCategory $jtlCategory, PrestaCategory $prestaCategory): PrestaCategory
+    {
+        $translations                = $this->createPrestaCategoryTranslations(...$jtlCategory->getI18ns());
+        $prestaCategory->id_parent   = $jtlCategory->getParentCategoryId()->getEndpoint();
+        $prestaCategory->active      = $jtlCategory->getIsActive();
+        $prestaCategory->position    = $jtlCategory->getSort();
+        $prestaCategory->level_depth = $jtlCategory->getLevel();
+
+        foreach ($translations as $key => $translation) {
+            $prestaCategory->name[$key]             = $translation['name'];
+            $prestaCategory->description[$key]      = $translation['description'];
+            $prestaCategory->meta_description[$key] = $translation['metaDescription'];
+            $prestaCategory->meta_keywords[$key]    = $translation['metaKeywords'];
+            $prestaCategory->link_rewrite[$key]     = $translation['url'];
+        }
+
+        return $prestaCategory;
+    }
+
+    protected function createPrestaCategoryTranslations(JtlCategoryI18n ...$jtlCategoryI18ns): array
+    {
+        $translations = [];
+        foreach ($jtlCategoryI18ns as $jtlCategoryI18n) {
+            $languageIso = $this->getPrestaLanguageIdFromIso($jtlCategoryI18n->getLanguageIso());
+
+            $langId                                   = $languageIso;
+            $translations[$langId]['name']            = $jtlCategoryI18n->getName();
+            $translations[$langId]['description']     = $jtlCategoryI18n->getDescription();
+            $translations[$langId]['metaDescription'] = $jtlCategoryI18n->getMetaDescription();
+            $translations[$langId]['metaKeywords']    = $jtlCategoryI18n->getMetaKeywords();
+            $translations[$langId]['url']             = $jtlCategoryI18n->getUrlPath();
+        }
+
+        return $translations;
+    }
+
+    /**
+     * @throws PrestaShopException
+     */
     public function delete(AbstractModel $model): AbstractModel
     {
-        $category = new \Category($data->getId()->getEndpoint());
+        $category = new PrestaCategory($model->getId()->getEndpoint());
 
         if (!$category->delete()) {
-            //throw new \Exception('Error deleting category with id: '.$data->getId()->getEndpoint());
+            throw new \Exception('Error deleting category with id: ' . $model->getId()->getEndpoint());
         }
 
-        return $data;
+        return $model;
     }
 
-    public function getStats()
+    public function statistic(): Statistic
     {
-        return $this->db->getValue(
-            '
-			SELECT COUNT(*) 
-			FROM ' . \_DB_PREFIX_ . 'category c
-			LEFT JOIN jtl_connector_link_category l ON c.id_category = l.endpoint_id
-            WHERE l.host_id IS NULL AND c.id_parent != 0 AND c.is_root_category = 0
-        '
-        );
+        $queryBuilder = new QueryBuilder();
+        $queryBuilder->setUsePrefix(false);
+
+        $sql = $queryBuilder
+            ->select('COUNT(*)')
+            ->from(\_DB_PREFIX_ . 'category', 'c')
+            ->leftJoin('jtl_connector_link_category', 'l', 'c.id_category = l.endpoint_id')
+            ->where('l.host_id IS NULL AND c.id_parent != 0 AND c.is_root_category = 0');
+
+        $result = $this->db->getValue($sql);
+
+        return (new Statistic())
+            ->setAvailable($result)
+            ->setControllerName($this->controllerName);
     }
 }
