27dd46b1a41d68ae35d8cfdd3991ac4ba1d75078
Patrick Gugelsberger <pgugelsberger@gmail.com>
2023-09-11 13:22:41 +0200

    implement ManufacturerController

--- src/Controller/ManufacturerController.php
+++ src/Controller/ManufacturerController.php
@@ -1,66 +1,195 @@
 <?php
 
+declare(strict_types=1);
+
 namespace jtl\Connector\Presta\Controller;
 
-class ManufacturerController extends AbstractController
+use Jtl\Connector\Core\Controller\DeleteInterface;
+use Jtl\Connector\Core\Controller\PullInterface;
+use Jtl\Connector\Core\Controller\PushInterface;
+use Jtl\Connector\Core\Model\AbstractModel;
+use Jtl\Connector\Core\Model\Identity;
+use Jtl\Connector\Core\Model\QueryFilter;
+use Jtl\Connector\Core\Model\Statistic;
+use jtl\Connector\Presta\Utils\QueryBuilder;
+use Manufacturer as PrestaManufacturer;
+use Jtl\Connector\Core\Model\Manufacturer as JtlManufacturer;
+use Jtl\Connector\Core\Model\ManufacturerI18n as JtlManufacturerI18n;
+
+class ManufacturerController extends AbstractController implements PushInterface, PullInterface, DeleteInterface
 {
-    public function pullData($data, $model, $limit = null)
+    /**
+     * @param QueryFilter $queryFilter
+     * @return array|AbstractModel[]
+     * @throws \PrestaShopDatabaseException
+     */
+    public function pull(QueryFilter $queryFilter): array
     {
-        $result = $this->db->executeS(
-            '
-			SELECT m.*
-			FROM ' . \_DB_PREFIX_ . 'manufacturer m
-			LEFT JOIN jtl_connector_link_manufacturer l ON m.id_manufacturer = l.endpoint_id
-            WHERE l.host_id IS NULL
-            LIMIT ' . $limit
+        $prestaManufacturers = $this->getNotLinkedEntities(
+            $queryFilter,
+            self::MANUFACTURER_LINKING_TABLE,
+            'manufacturer',
+            'id_manufacturer'
         );
 
-        $return = [];
+        $jtlManufacturers = [];
 
-        foreach ($result as $data) {
-            $model = $this->mapper->toHost($data);
 
-            $return[] = $model;
+        foreach ($prestaManufacturers as $prestaManufacturer) {
+            $jtlManufacturers[] = $this->createJtlManufacturers(
+                new PrestaManufacturer($prestaManufacturer['id_manufacturer'])
+            );
         }
 
-        return $return;
+        return $jtlManufacturers;
     }
 
-    public function pushData($data)
+    /**
+     * @param PrestaManufacturer $prestaManufacturer
+     * @return JtlManufacturer
+     */
+    protected function createJtlManufacturers(PrestaManufacturer $prestaManufacturer): JtlManufacturer
     {
-        $manufacturer = $this->mapper->toEndpoint($data);
+        $jtlManufacturer = (new JtlManufacturer())
+            ->setId(new Identity((string)$prestaManufacturer->id))
+            ->setName($prestaManufacturer->name)
+            ->setI18ns(...$this->createJtlManufacturerI18ns($prestaManufacturer));
 
-        $manufacturer->name = \str_replace('#', '', $manufacturer->name);
-
-        $manufacturer->save();
-
-        $id = $manufacturer->id;
-
-        $data->getId()->setEndpoint($id);
-
-        return $data;
+        return $jtlManufacturer;
     }
 
-    public function deleteData($data)
+    /**
+     * @param PrestaManufacturer $manufacturer
+     * @return array
+     * @throws \PrestaShopDatabaseException
+     */
+    protected function createJtlManufacturerI18ns(PrestaManufacturer $manufacturer): array
     {
-        $manufacturer = new \Manufacturer($data->getId()->getEndpoint());
+        $jtlI18ns = [];
+
+        foreach (\Language::getLanguages() as $language) {
+            $langId = $language['id_lang'];
+
+            $jtlI18n = (new JtlManufacturerI18n())
+                ->setLanguageIso($this->getJtlLanguageIsoFromLanguageId($langId))
+                ->setDescription($manufacturer->description[$langId])
+                ->setMetaDescription($manufacturer->meta_description[$langId])
+                ->setMetaKeywords($manufacturer->meta_keywords[$langId])
+                ->setTitleTag($manufacturer->meta_title[$langId]);
+
+            $jtlI18ns[] = $jtlI18n;
+        }
+        return $jtlI18ns;
+    }
+
+    /**
+     * @param AbstractModel $jtlManufacturer
+     * @return AbstractModel
+     * @throws \PrestaShopDatabaseException
+     * @throws \PrestaShopException
+     */
+    public function push(AbstractModel $jtlManufacturer): AbstractModel
+    {
+        /** @var JtlManufacturer $jtlManufacturer */
+        $endpoint = $jtlManufacturer->getId()->getEndpoint();
+        $isNew    = $endpoint === '';
+
+        if (!$isNew) {
+            $prestaManufacturer = $this->createPrestaManufacturer($jtlManufacturer, new PrestaManufacturer($endpoint));
+            if (!$prestaManufacturer->update()) {
+                throw new \RuntimeException('Error updating manufacturer' . $jtlManufacturer->getName());
+            }
+
+            return $jtlManufacturer;
+        }
+
+        $prestaManufacturer = $this->createPrestaManufacturer($jtlManufacturer, new PrestaManufacturer());
+        if (!$prestaManufacturer->add()) {
+            throw new \RuntimeException('Error uploading manufacturer' . $jtlManufacturer->getName());
+        }
+
+        return $jtlManufacturer;
+    }
+
+    /**
+     * @param JtlManufacturer $jtlManufacturer
+     * @param PrestaManufacturer $prestaManufacturer
+     * @return PrestaManufacturer
+     */
+    protected function createPrestaManufacturer(
+        JtlManufacturer $jtlManufacturer,
+        PrestaManufacturer $prestaManufacturer
+    ): PrestaManufacturer {
+
+        $translations               = $this->createPrestaManufacturerTranslations(...$jtlManufacturer->getI18ns());
+        $prestaManufacturer->active = 1;
+
+        foreach ($translations as $key => $translation) {
+            $prestaManufacturer->name[$key]             = $jtlManufacturer->getName();
+            $prestaManufacturer->description[$key]      = $translation['description'];
+            $prestaManufacturer->meta_title[$key]       = $translation['meta_title'];
+            $prestaManufacturer->meta_keywords[$key]    = $translation['meta_keywords'];
+            $prestaManufacturer->meta_description[$key] = $translation['meta_description'];
+        }
+
+        return $prestaManufacturer;
+    }
+
+    /**
+     * @param JtlManufacturerI18n ...$jtlManufacturerI18ns
+     * @return array
+     * @throws \PrestaShopDatabaseException
+     */
+    protected function createPrestaManufacturerTranslations(JtlManufacturerI18n ...$jtlManufacturerI18ns): array
+    {
+        $translations = [];
+        foreach ($jtlManufacturerI18ns as $jtlManufacturerI18n) {
+            $languageIso = $this->getPrestaLanguageIdFromIso($jtlManufacturerI18n->getLanguageIso());
+
+            $langId                                    = $languageIso;
+            $translations[$langId]['description']      = $jtlManufacturerI18n->getDescription();
+            $translations[$langId]['meta_title']       = $jtlManufacturerI18n->getTitleTag();
+            $translations[$langId]['meta_keywords']    = $jtlManufacturerI18n->getMetaKeywords();
+            $translations[$langId]['meta_description'] = $jtlManufacturerI18n->getMetaDescription();
+        }
+
+        return $translations;
+    }
+
+    /**
+     * @param AbstractModel $model
+     * @return AbstractModel
+     * @throws \PrestaShopException
+     */
+    public function delete(AbstractModel $model): AbstractModel
+    {
+        $manufacturer = new \Manufacturer($model->getId()->getEndpoint());
 
         if (!$manufacturer->delete()) {
-            //throw new \Exception('Error deleting manufacturer with id: '.$data->getId()->getEndpoint());
+            throw new \RuntimeException('Error deleting manufacturer with id: ' . $model->getId()->getEndpoint());
         }
 
-        return $data;
+        return $model;
     }
 
-    public function getStats()
+    /**
+     * @return Statistic
+     */
+    public function statistic(): Statistic
     {
-        return $this->db->getValue(
-            '
-			SELECT COUNT(*) 
-			FROM ' . \_DB_PREFIX_ . 'manufacturer m
-			LEFT JOIN jtl_connector_link_manufacturer l ON m.id_manufacturer = l.endpoint_id
-            WHERE l.host_id IS NULL
-        '
-        );
+        $queryBuilder = new QueryBuilder();
+        $queryBuilder->setUsePrefix(false);
+
+        $sql = $queryBuilder
+            ->select('COUNT(*)')
+            ->from(\_DB_PREFIX_ . 'manufacturer', 'm')
+            ->leftJoin(self::MANUFACTURER_LINKING_TABLE, 'l', 'm.id_manufacturer = l.endpoint_id')
+            ->where('l.host_id IS NULL');
+
+        $result = $this->db->getValue($sql);
+
+        return (new Statistic())
+            ->setAvailable((int)$result)
+            ->setControllerName($this->controllerName);
     }
 }
