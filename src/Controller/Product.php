8d5197d5bc64a4d405d4612d1c53fb9b6b5a0cfd
Marius Brandt <Marius.Brandt@jtl-software.com>
2024-01-23 15:26:18 +0100

    fix method Attribute::save() not found in product push

--- src/Controller/Product.php
+++ src/Controller/Product.php
@@ -2,7 +2,6 @@
 
 namespace jtl\Connector\Presta\Controller;
 
-use Attribute;
 use AttributeGroup;
 use Combination;
 use Context;
@@ -384,7 +383,7 @@ class Product extends BaseController
                             }
                         }
 
-                        $val                     = new Attribute($valId);
+                        $val                     = new \ProductAttribute($valId);
                         $val->name               = $valNames;
                         $val->id_attribute_group = $attrGrpId;
                         $val->position           = $value->getSort();
